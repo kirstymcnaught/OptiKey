c62ba3e1b47c37875b47d746ad930e7ac2c8379b
K McNaught <kmcnaught.git@gmail.com>
2017-10-20 12:37:48 +0100

    Remove backAction if first keyboard is dynamic menu

--- src/JuliusSweetland.OptiKey/UI/ViewModels/MainViewModel.cs
+++ src/JuliusSweetland.OptiKey/UI/ViewModels/MainViewModel.cs
@@ -384,7 +384,7 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
                     break;
 
                 case Enums.Keyboards.DynamicKeyboard:
-                    Keyboard = new DynamicKeyboardSelector(backAction, 0);
+                    Keyboard = new DynamicKeyboardSelector(() => { }, 0);
                     break;
 
                 case Enums.Keyboards.Menu:
